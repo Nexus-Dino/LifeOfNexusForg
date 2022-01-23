885ddccfd4dacd69982a443d65399961127c9abd
IOGitubPranav <86781290+Nexus-Dino@users.noreply.github.com>
2022-01-23 22:49:19 +0000

    start from scratch

--- src/main/java/com/nexusdino/lifeofnexus/core/init/ItemInit.java
+++ src/main/java/com/nexusdino/lifeofnexus/core/init/ItemInit.java
@@ -1,21 +1,9 @@
 package com.nexusdino.lifeofnexus.core.init;
 
-import com.nexusdino.lifeofnexus.LifeOfNexus;
+public class ItemInit {
 
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.Item;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
-
-public final class ItemInit {
-
-	private ItemInit() {
+	public ItemInit() {
+		// TODO Auto-generated constructor stub
 	}
 
-	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS,
-			LifeOfNexus.MOD_ID);
-
-	public static final RegistryObject<Item> SCYTHONITE_INGOT = ITEMS.register("scythonite_ingot",
-			() -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC).fireResistant()));
 }
